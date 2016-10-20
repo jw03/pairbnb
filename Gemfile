daca5b0a64017611437435e9c6b81b32ef773fda
jw03 <james.wright@assertis.co.uk>
2016-10-20 13:02:14 +0800

    followed steps in ssy tutorial for aws

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'figaro'
 #for image upload
 gem 'carrierwave', github: 'carrierwaveuploader/carrierwave'
 gem 'mini_magick'
-gem "fog"
+gem 'fog'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,8 +5,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   version :large do
     process resize_to_limit: [800, 800]
